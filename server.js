d7eedfafe803a41c322b385bc6f546ba7ac831fc
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-05-13 22:45:49 +0100

    route creation

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require("multer")
+var upload = multer()
 
-// require and use "multer"...
 
 var app = express();
 
@@ -17,6 +18,11 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
+  let upfile = req.file
+  res.json({name: upfile.originalname, type: upfile.mimetype, size: upfile.size})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
